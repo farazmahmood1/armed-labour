c5b22874472db47f9ae356631e990efafd59677c
ahmadali333 <ahmadali.cali8@gmail.com>
2026-01-29 13:05:24 -0400

    updated the code

--- src/services/firebase/init.ts
+++ src/services/firebase/init.ts
@@ -6,12 +6,13 @@ import { getStorage } from 'firebase/storage';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDkqSpFkKlRigyyR732gNjSTICFsSjYdkM",
-  authDomain: "kaarigar360.firebaseapp.com",
-  projectId: "kaarigar360",
-  storageBucket: "kaarigar360.firebasestorage.app",
-  messagingSenderId: "601840315116",
-  appId: "1:601840315116:android:922c14a626df6f711c93c9"
+  apiKey: "AIzaSyBDhRu_7L42mPBna9NIU6QUJxz42QTpm9Q",
+  authDomain: "huzaifa-karigar.firebaseapp.com",
+  projectId: "huzaifa-karigar",
+  storageBucket: "huzaifa-karigar.firebasestorage.app",
+  messagingSenderId: "798815308866",
+  appId: "1:798815308866:web:50fe4cb95c444613e071db",
+  measurementId: "G-GZ507XTC4Q"
 };
 
 let app: any = null;
